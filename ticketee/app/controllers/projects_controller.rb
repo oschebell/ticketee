672a8c261a1e6b971ee4c6f3995afa09741898d7
oschebell <oschebell@gmail.com>
2016-02-21 15:39:49 +1100

    Create project model

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -1,2 +1,10 @@
 class ProjectsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
 end
--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "projects#index"
+
+  resources :projects
 end
