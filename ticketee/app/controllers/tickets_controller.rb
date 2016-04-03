ddec8156a53a17d3aaa4605cf03a52fde31de204
oschebell <oschebell@gmail.com>
2016-04-03 23:52:29 +1000

    Begin work on linking tickets and users upon ticket creation

--- ticketee/app/controllers/tickets_controller.rb
+++ ticketee/app/controllers/tickets_controller.rb
@@ -10,6 +10,8 @@ class TicketsController < ApplicationController
 
   def create
     @ticket = @project.tickets.build(ticket_params)
+    @ticket.author = current_user
+    
     if @ticket.save
       flash[:notice] = "Ticket has been created."
       redirect_to [@project, @ticket]
--- ticketee/spec/features/creating_tickets_spec.rb
+++ ticketee/spec/features/creating_tickets_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
   RSpec.feature "Users can create new tickets" do
+    let(:user) {FactoryGirl.create(:user)}
+
     before do
+      login_as(user)
       project = FactoryGirl.create(:project, name: "Internet Explorer")
       visit project_path(project)
       click_link "New Ticket"
@@ -11,6 +14,9 @@ require "rails_helper"
       fill_in "Description", with: "My pages are ugly!"
       click_button "Create Ticket"
       expect(page).to have_content "Ticket has been created."
+      within("#ticket") do
+        expect(page).to have_content "Author: #{user.email}"
+      end
     end
 
     scenario "when providing invalid attributes" do
