af6481932056b70d475524a3712efdc45dc79f7c
oschebell <oschebell@gmail.com>
2016-04-06 14:52:21 +1000

    Only admins can delete or create projects

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -2,67 +2,35 @@ class ProjectsController < ApplicationController
 
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-
   def index
     @projects = Project.all
   end
 
-  def new
-  @project = Project.new
-  end
-
   def show
-  @project = Project.find(params[:id])
   end
 
   def edit
-  @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
-    @project.update(project_params)
-
-    if @project.save
+    if @project.update(project_params)
       flash[:notice] = "Project has been updated."
-    redirect_to @project
+      redirect_to @project
     else
       flash.now[:alert] = "Project has not been updated."
       render "edit"
     end
-  end
-
-
-  def create
-    @project = Project.new(project_params)
-
-    if @project.save
-      flash[:notice] = "Project has been created."
-      redirect_to @project
-    else
-      flash.now[:alert] = "Project has not been created."
-      render "new"
-    end
-  end
-
-  def destroy
-    @project = Project.find(params[:id])
-    @project.destroy
-    flash[:notice] = "Project has been deleted."
-    redirect_to projects_path
-  end
-
 end
 
 private
-
-def project_params
-  params.require(:project).permit(:name, :description)
-end
-
-def set_project
+  def set_project
     @project = Project.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     flash[:alert] = "The project you were looking for could not be found."
     redirect_to projects_path
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-      root "application#index"
+   root "application#index"
+   resources :projects, only: [:new, :create, :destroy]
   end
 
   devise_for :users
   root "projects#index"
 
-  resources :projects do
+  resources :projects, only: [ :index, :show, :edit, :update ] do
     resources :tickets
   end
+
+
 end
