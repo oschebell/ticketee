a0bdd5306351a1543f8f53ea57e7155444cc46fa
oschebell <oschebell@gmail.com>
2016-04-12 11:51:24 +1000

    Add ability for admins to edit and update users

--- ticketee/app/controllers/admin/users_controller.rb
+++ ticketee/app/controllers/admin/users_controller.rb
@@ -1,8 +1,31 @@
 class Admin::UsersController < Admin::ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
       @users = User.order(:email)
+  end
+
+  def show
+    #empty
+  end
+
+  def edit
+    #empty
+  end
+
+  def update
+    if params[:user][:password].blank?
+        params[:user].delete(:password)
     end
+
+    if @user.update(user_params)
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "User has not been updated."
+      render "edit"
+    end
+  end
 
   def new
     @user = User.new
@@ -19,8 +42,14 @@ class Admin::UsersController < Admin::ApplicationController
   end
 end
 
+
+
 private
   def user_params
     params.require(:user).permit(:email, :password, :admin)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
