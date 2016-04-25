93834f337525f107bef6e376f4dc6e13c8e5160d
oschebell <oschebell@gmail.com>
2016-04-25 23:04:55 +1000

    Only admins and managers can edit projects

--- ticketee/app/policies/project_policy.rb
+++ ticketee/app/policies/project_policy.rb
@@ -1,14 +1,19 @@
 class ProjectPolicy < ApplicationPolicy
     class Scope < Scope
+
       def resolve
         return scope.none if user.nil?
         return scope.all if user.admin?
 
         scope.joins(:roles).where(roles: {user_id: user})
+      end
     end
 
     def show?
-    user.try(:admin?) || record.roles.exists?(user_id: user)
+      user.try(:admin?) || record.roles.exists?(user_id: user)
+    end
+
+    def update?
+      user.try(:admin?) || record.roles.exists?(user_id: user, role: 'manager')
     end
   end
-end
