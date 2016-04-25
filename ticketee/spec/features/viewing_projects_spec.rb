b89e3d4e444df0c3ca9711fae7bc8c644d479119
oschebell <oschebell@gmail.com>
2016-04-25 20:45:01 +1000

    Don't list projects that a user doesn't have permission to see

--- ticketee/spec/features/viewing_projects_spec.rb
+++ ticketee/spec/features/viewing_projects_spec.rb
@@ -15,4 +15,10 @@ require "rails_helper"
       click_link "Sublime Text 3"
       expect(page.current_url).to eq project_url(project)
     end
+
+    scenario "unless they do not have permission" do
+      FactoryGirl.create(:project, name: "Hidden")
+      visit "/"
+      expect(page).not_to have_content "Hidden"
+    end
   end
